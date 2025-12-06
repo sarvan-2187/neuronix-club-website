2790c22e0d117ae3d427c23c5f36a519a5530017
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-12-06 17:34:42 +0530

    Updated Team Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
       <NeuronixHeader />
       <NeuronixHero />
       <NeuronixAbout />
-      <NeuronsBook />
       <NeuronixFAQ />
       <NeuronixFooter />
     </main>
